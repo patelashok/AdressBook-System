23d511307170006653b7d47341a15d2cd1dc5515
patelashok <ashok.patel2917@gmail.com>
2021-06-01 08:47:28 +0530

    update:add multiple person address using collection

--- Person.java
+++ Person.java
@@ -1,27 +1,63 @@
 package addressbook;
 import java.util.ArrayList;
+import java.util.*;
 
 public class Person {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-        ArrayList<String> p=new ArrayList<String>();
+        ArrayList<String> p = new ArrayList<String>();
+        public static Scanner sc = new Scanner(System.in);
 
-        p.add("Ashok");
-        p.add("Ramesh");
-        p.add("Patel");
-        p.add("Ram");
-        p.add("Sita");
+        public static String getUserInput() {
+            String fName,lName;
+            String Address,city;
+            String state;
+            String zip,phone;
+            String email;
 
-        System.out.println(p);
+            System.out.println("Enter first and last name:");
+            fName = sc.next();
+            lName = sc.next();
+            System.out.println("Enter Address:");
+            Address = sc.next();
+            System.out.println("Enter City:");
+            city = sc.next();
+            System.out.println("Enter state:");
+            state = sc.next();
+            System.out.println("Enter zip:");
+            zip = sc.next();
+            System.out.println("Enter phone:");
+            phone = sc.next();
+            System.out.println("Enter email:");
+            email = sc.next();
 
-        //Adding through index value
-        p.add(3, "Syam");
+            ArrayList<String> p = new ArrayList<>();
+            p.add(fName);
+            p.add( lName);
+            p.add( Address);
+            p.add( city);
+            p.add(state);
+            p.add(zip);
+            p.add( phone);
+            p.add( email);
 
-        //Printing elements
-        System.out.println(p);
+            System.out.println("Do you want to add contact in address book (Y/N)");
+            return sc.next();
+        }
+
+    public static void main(String args[]) {
+
+        System.out.println("Do you want to add contact in address book (Y/N)");
+        String userDecision = sc.next();
+
+        while(userDecision.equalsIgnoreCase("Y")) {
+            userDecision = getUserInput();
+        }
+
+
     }
+
 }
+
 
 
 
